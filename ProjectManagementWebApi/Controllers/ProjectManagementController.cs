40f8de457fef29340640cde3e1e8467fe24c2dac
bibha <bibha@DESKTOP-QI4A1OU>
2022-04-11 19:32:28 -0600

    Add project files.

---
+++ ProjectManagementWebApi/Controllers/ProjectManagementController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagementWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectManagementController : ControllerBase {
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ProjectManagementController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProject")]
+        public IEnumerable<ProjectDetail> Get()
+        {
+           List <ProjectDetail> prj = new List<ProjectDetail>();
+
+            prj.Add(new ProjectDetail { Projecthr = 1, ProjectID = 2, ProjectSummary = "Test" });
+
+            return prj;
+
+
+        }
+
+    }
+    
+
+
+    }
+
---
+++ ProjectManagementWebApi/ProjectDetail.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectManagementWebApi
+{
+    public class ProjectDetail
+
+    { 
+
+     public int ProjectID { get; set; }
+
+     public string? ProjectSummary { get; set; }
+
+     public int? Projecthr{ get; set; }
+
+
+
+
+
+    }
+}
